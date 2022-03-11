93afc30bdd5a51682c3d3db539e509497786531c
Leandro Freire <leandrocabral321@gmail.com>
2022-03-11 20:42:17 -0300

    adicionando spring

---
+++ u1_exercicio3/cadpessoas_leandro/src/test/java/br/com/leandro/cadpessoas/CadpessoasLeandroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandro.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasLeandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
