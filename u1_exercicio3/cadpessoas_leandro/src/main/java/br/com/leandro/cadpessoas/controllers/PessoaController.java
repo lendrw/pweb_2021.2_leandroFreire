3aeaaaf2e3116dd0410d3ddfba7db3d61b9761b1
lendrw <leandro.cabral@alunos.ufersa.edu.br>
2022-03-26 20:15:18 -0300

    r do crud

--- u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/controllers/PessoaController.java
@@ -1,9 +1,12 @@
 package br.com.leandro.cadpessoas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.leandro.cadpessoas.repositories.PessoaRepository;
 
@@ -16,7 +19,14 @@ public class PessoaController{
 
     @GetMapping
     public String index(){
-        return index.html;
+        return "index.html";
     }
     
+    @GetMapping("/listarPessoas")
+    public <Pessoa> ModelAndView listarPessoas(){
+        List<Pessoa> lista = pessoaRepo.findAll();
+        ModelAndView mav = ModelAndView("listarPessoas");
+        mav.addObject("pessoas", lista);
+        return mav;
+    }
 }
