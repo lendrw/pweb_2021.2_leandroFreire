0270e4e8b28e4c5f914c52baeaba6e8013dfe8ea
lendrw <leandro.cabral@alunos.ufersa.edu.br>
2022-03-26 19:33:36 -0300

    r do crud

--- u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,22 @@
 package br.com.leandro.cadpessoas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.leandro.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController{
 
+    @Autowired
+    PessoaRepository pessoaRepo;
+
+    @GetMapping
+    public String index(){
+        return index.html;
+    }
+    
 }
--- u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/repositories/PessoaRepository.java
+++ u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/repositories/PessoaRepository.java
@@ -1,8 +1,9 @@
 package br.com.leandro.cadpessoas.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PessoaRepository extends JpaRepository<Pessoa, Long>{
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
     
 }
