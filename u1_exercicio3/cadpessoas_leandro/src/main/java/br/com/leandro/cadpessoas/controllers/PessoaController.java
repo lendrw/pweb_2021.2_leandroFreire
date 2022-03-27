150213c7aee0164f4b3fbefe49425c42bd76eac6
lendrw <leandro.cabral@alunos.ufersa.edu.br>
2022-03-27 19:39:26 -0300

    ajustando pastas

--- u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/controllers/PessoaController.java
@@ -1,5 +1,6 @@
 package br.com.leandro.cadpessoas.controllers;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/repositories/PessoaRepository.java
+++ u1_exercicio3/cadpessoas_leandro/src/main/java/br/com/leandro/cadpessoas/repositories/PessoaRepository.java
@@ -3,6 +3,8 @@ package br.com.leandro.cadpessoas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.leandro.cadpessoas.model.Pessoa;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
     
