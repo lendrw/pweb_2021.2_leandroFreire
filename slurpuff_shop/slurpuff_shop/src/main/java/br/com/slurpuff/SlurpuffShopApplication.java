2b52b73f83d6b34da7bcb2db8059f1ad5b08ba42
lendrw <88214669+lendrw@users.noreply.github.com>
2022-04-19 17:37:34 -0300

    iniciando projeto da loja de doces

---
+++ slurpuff_shop/slurpuff_shop/src/main/java/br/com/slurpuff/SlurpuffShopApplication.java
@@ -0,0 +1,13 @@
+package br.com.slurpuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlurpuffShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlurpuffShopApplication.class, args);
+	}
+
+}
---
+++ slurpuff_shop/slurpuff_shop/src/test/java/br/com/slurpuff/SlurpuffShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.slurpuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlurpuffShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
